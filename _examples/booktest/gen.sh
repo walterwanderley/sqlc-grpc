b1ab6473f8d2544b0a54df58a3463f70d6849cfa
Walter Wanderley <walterwanderley@gmail.com>
2021-11-23 00:23:02 -0300

    update examples

--- _examples/booktest/gen.sh
+++ _examples/booktest/gen.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 
 go install github.com/kyleconroy/sqlc/cmd/sqlc@latest
+go install github.com/bufbuild/buf/cmd/buf@latest
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest
